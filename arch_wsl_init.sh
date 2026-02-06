60d37fc761385e7dda86c91cce3a501843e04cee
vicmaeg <vmartos@outlook.com>
2026-02-06 21:49:42 +0100

    feat(tmux): use sesh and change tmux config to use defaults

--- arch_wsl_init.sh
+++ arch_wsl_init.sh
@@ -3,4 +3,6 @@
 sudo pacman -S base-devel git ripgrep fd neovim tree-sitter-cli \
   dotnet-sdk dotnet-runtime-8.0 aspnet-runtime-8.0 \
   starship zoxide fzf stylua unzip wget curl tar lazygit lazydocker \
-  eza
+  eza stow tmux bat
+
+yay -S sesh-bin
