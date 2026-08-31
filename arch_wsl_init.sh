025af5a0de77e13179e9ae37a88ea4187f76348e
vicmaeg <vmartos@outlook.com>
2026-08-31 20:35:44 +0200

    add git delta as the pager in gitconfig

--- arch_wsl_init.sh
+++ arch_wsl_init.sh
@@ -3,4 +3,4 @@
 sudo pacman -S base-devel git ripgrep fd neovim tree-sitter-cli \
   dotnet-sdk dotnet-runtime-8.0 aspnet-runtime-8.0 \
   starship zoxide fzf stylua unzip wget curl tar lazygit lazydocker \
-  eza stow tmux bat
+  eza stow tmux bat git-delta
